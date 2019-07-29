e94863d56c20e7d4eae14df1a6bd25129de5b5bf
michaelrhodes <spam@michaelrhod.es>
2019-07-29 11:51:10 +1000

    Update version

--- install.js
+++ install.js
@@ -1,13 +1,12 @@
 var os = require('os')
 var path = require('path')
 var build = require('bin-build')
-
-var HEAD = '7e9ef877c35714d785328962ad875e629a8dd795' 
+var pkg = require('./package.json')
 
 build()
-  .src('https://github.com/dropbox/lepton/archive/' + HEAD + '.tar.gz')
+  .src('https://github.com/dropbox/lepton/archive/' + pkg.version + '.tar.gz')
   .cmd('cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=/ .')
-  .cmd('make' + (os.cpus().length > 1 ? ' -j8' : ''))
+  .cmd('make -j' + os.cpus().length)
 	.cmd('make DESTDIR=' + __dirname + ' install')
   .run(function (err) {
     if (err) return console.error(err)
